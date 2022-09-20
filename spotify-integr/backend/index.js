c8aacbce1aacd9fd9e10bc94a08dc93576f61989
Arturs Olekss <arturs.olekss@gmail.com>
2022-09-20 18:01:29 +0300

    15_09

--- spotify-integr/backend/index.js
+++ spotify-integr/backend/index.js
@@ -33,36 +33,16 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.json());
 
-const client_id = '899582c259f64dcebc05e6bb4d83828d';
+let client_id; // = '899582c259f64dcebc05e6bb4d83828d';
 const redirect_uri = 'http://localhost:5000/callback';
-const client_secret = '677d46bca6664e88b7c9cbd8325c321c';
-
-app.get('/', function(req, res) {
-    fs.readFile('../frontend/index.html', function(err, html) {
-        if (err) {
-            throw err;
-        }
-        res.write(html)
-        res.end();
-    })
-})
-
-app.get('/scripts/script.js', function(req, res) {
-    fs.readFile('scripts/script.js', function(err, html) {
-        if (err) {
-            throw err;
-        }
-        res.write(html)
-        res.end();
-    })
-})
+let client_secret; // = '677d46bca6664e88b7c9cbd8325c321c';
 
 
 // let client_secret, client_id;
 app.get('/login', function(req, res) {
 
-    // client_id = req.query.client_id;
-    // client_secret = req.query.client_secret;
+    client_id = req.query.client_id;
+    client_secret = req.query.client_secret;
     var state = randomstring.generate(16);
     var scope = 'user-read-private user-read-email';
 
@@ -102,38 +82,27 @@ app.get('/callback', function(req, res) {
         request.post(authOptions, function(error, response, body) {
             if (!error && response.statusCode === 200) {
                 const access_token = body.access_token;
-                res.send({
-                    'access_token': access_token
-                });
+                const base_url = "http://localhost:5000";
+                res.redirect(base_url + "?token=" + access_token);
             }
         });
 
     }
 });
 
-app.get('/getArtist', async function(req, res) {
-    const name = req.query.name;
-    const token = req.query.token;
-    console.log(token);
-    const authOptions = {
-        url: "https://api.spotify.com/v1/search?q=" + name + "&type=artist",
-        headers: {
-            'Authorization': 'Bearer ' + token
-        },
-        json: true
-    }
-
-    request(authOptions, function(error, response, body) {
-        if (!error && res.statusCode == 200) {
-            res.send(JSON.stringify(body));
-        } else {
-            res.send(err);
-        }
+app.get('/', function(req, res) {
+    fs.readFile('../frontend/artistinfo.html', function(err, html) {
+        res.write(html);
+        res.end();
     })
 })
 
-
-
+app.get('/script', function(req, res) {
+    fs.readFile('scripts/script.js', function(err, script) {
+        res.write(script);
+        res.end();
+    })
+})
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
--- spotify-integr/backend/scripts/script.js
+++ spotify-integr/backend/scripts/script.js
@@ -1,16 +1,83 @@
-const signIn = () => {
-    const client_id = $("#clientID").val();
-    const client_secret = $("#clientSecret").val();
-    const url = "http://localhost:5000/login?client_id=" +
-        client_id + "&client_secret=" + client_secret;
-    $.get(url, (data, status) => {
-        console.log(status);
-        console.log(data);
+const onGetArtist = () => {
+    const name = $("#artistName").val();
+    const token = $("#token").val();
+
+    $.ajax({
+        url: "https://api.spotify.com/v1/search?q=" + name + "&type=artist",
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        json: true,
+        success: (result) => {
+            const artist = result.artists.items.find((artist) => {
+                return artist.name === name;
+            })
+            if (artist === undefined)
+                return;
+            console.log(artist);
+            $("#name").text(artist.name);
+
+            let genres = "";
+            for (let i = 0; i < artist.genres.length; i++) {
+                if (genres === "")
+                    genres = artist.genres[i];
+                else
+                    genres += (", " + artist.genres[i]);
+            }
+            $("#genres").text(genres);
+            $("#followers").text(artist.followers.total);
+
+            const image = $("#artistImage");
+            image.attr('src', artist.images[0].url);
+
+        }
+    })
+}
+
+const onGetAlbum = () => {
+    const name = $("#albumName").val();
+    const token = $("#token").val();
+
+    $.ajax({
+        url: "https://api.spotify.com/v1/search?q=" + name + "&type=album",
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        json: true,
+        success: (result) => {
+            const album = result.albums.items.find((album) => {
+                return album.name === name;
+            })
+            albumDetails(album.id, token);
+        }
     })
 }
 
 
+const albumDetails = function(id, token) {
+    const url = "https://api.spotify.com/v1/albums/";
+
+    $.ajax({
+        url: url + id,
+        headers: {
+            'Authorization': 'Bearer ' + token
+        },
+        json: true,
+        success: (result) => {
+            console.log(result);
+        }
+    })
+}
+
+
+
+
 //On load
 $(function() {
-    $("#signidBtn").on("click", signIn);
+    $("#getArtistInfo").on("click", onGetArtist);
+    $("#getAlbumInfo").on("click", onGetAlbum);
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const tokenVal = urlParams.get("token");
+    $("#token").val(tokenVal);
 });
